3a76a49cdf872dbf78c4428423b6d242670f47bc
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-09-17 22:46:14 +0200

    adds basic Header and Button components

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <button @click="onClick()" :style="{ background: color }" class="btn">{{ text }}</button>
+</template>
+
+<script>
+    export default {
+        name: 'ButtonComponent',
+        props: {
+            text: String,
+            color: String,
+        },
+        methods: {
+            onClick() {
+                // TODO: handle click event
+                console.log('clicked');
+            }
+        }
+    }
+</script>
